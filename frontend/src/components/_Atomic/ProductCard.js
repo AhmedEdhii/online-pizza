45f72a7c1846bf658c79f75531da9a1f71f82e79
ebrahimbaig <BAIG.EBRAHIM@GMAIL.COM>
2022-05-27 02:20:38 +0500

    Product Card and show products

--- frontend/src/components/_Atomic/ProductCard.js
+++ frontend/src/components/_Atomic/ProductCard.js
@@ -1,10 +1,80 @@
-import { Typography } from '@mui/material'
+import {
+    Typography, Card, CardMedia, CardHeader, CardContent, CardActions, Collapse, IconButton, Grid, Button, Box, Item
+} from '@mui/material'
 import React from 'react'
 
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+
+
+
 function ProductCard() {
-  return (
-    <Typography>This is a product Cardx</Typography>
-  )
+    return (
+        <Card sx={{
+            p: 2,
+            margin: 'auto',
+            maxWidth: 500,
+            flexGrow: 1,
+            borderRadius: '1.5rem',
+            boxShadow: '0 8px 24px 0 rgba(0,0,0,0.12)'
+        }}>
+
+            <Grid container spacing={2} sx={{ alignItems: 'center' }}>
+
+                <Grid item xs={12} sm container >
+                    <Grid item xs container direction="column" spacing={2}>
+                        <Grid item xs>
+                            <CardContent>
+                                <Typography gutterBottom variant="h6" component="div" sx={{ fontWeight: 'bold' }}>
+                                    Fajita Chicken Supreme
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Buffalo Chicken, Tikka, Sweet Corns, Green
+                                    Onions, Jalapenos ,BBQ Ranch Sauce And
+                                    Mozzarella
+                                </Typography>
+
+                            </CardContent>
+                            <CardContent>
+                                <Typography gutterBottom variant="h6" component="div" >
+                                    Rs 299
+                                </Typography>
+
+                            </CardContent>
+
+                            <IconButton size='large' color='warning' ><AddCircleIcon />
+                            </IconButton>
+
+                            {/* <CardActions>
+                                <Button size="small">Share</Button>
+                                <Button size="small">Learn More</Button>
+                            </CardActions> */}
+                        </Grid>
+                    </Grid>
+                </Grid>
+
+                <Grid item sx={{ height: '100%', margin: 'auto', p: 2, }}>
+
+
+
+                    <CardMedia
+                        component="img"
+                        alt="green iguana"
+                        height="170"
+                        image="images/pizza.jpg"
+                    />
+
+
+
+                </Grid>
+
+            </Grid>
+
+        </Card>
+
+
+
+
+    )
 }
 
 export default ProductCard
--- frontend/src/components/layout/ShowProducts.js
+++ frontend/src/components/layout/ShowProducts.js
@@ -1,10 +1,52 @@
+import { Container, Divider, Grid, Typography } from '@mui/material'
 import React from 'react'
 import ProductCard from '../_Atomic/ProductCard'
 
 
 function ShowProducts() {
   return (
-    <ProductCard/>
+      
+    <>
+    <Container display="flex" sx={{margin: 'auto'}}>
+        <Typography variant='h2' sx={{ fontWeight: 'bold', marginBottom:5 }}>Pizzas</Typography>
+        <Divider sx={{ marginBottom:5}} />
+
+     <Grid  display="flex" container rowSpacing={5} columnSpacing={{ xs: 1, sm: 3, md: 3 }} sx={{marginBottom: 8}}>
+         <Grid item xs={6}>
+         <ProductCard />
+         </Grid>
+         <Grid item xs={6}>
+         <ProductCard />
+         </Grid>
+         <Grid item xs={6}>
+         <ProductCard />
+         </Grid>
+         <Grid item xs={6}>
+         <ProductCard />
+         </Grid>
+        
+    </Grid>
+
+    <Typography variant='h2' sx={{ fontWeight: 'bold', marginBottom:5 }}>Drinks</Typography>
+        <Divider sx={{ marginBottom:5}} />
+
+     <Grid  display="flex" container rowSpacing={5} columnSpacing={{ xs: 1, sm: 3, md: 3 }} sx={{marginBottom: 8}}>
+         <Grid item xs={6}>
+         <ProductCard />
+         </Grid>
+         <Grid item xs={6}>
+         <ProductCard />
+         </Grid>
+         <Grid item xs={6}>
+         <ProductCard />
+         </Grid>
+         <Grid item xs={6}>
+         <ProductCard />
+         </Grid>
+        
+    </Grid>
+    </Container>
+    </>
   )
 }
 
