50744ce7e71433830506701d01c8de123dc50e80
Pawan Kumar <64681287+pawan5627@users.noreply.github.com>
2022-06-01 04:59:54 +0500

    Banner image src local

--- frontend/src/components/_Atomic/imageslide/Slider_image.js
+++ frontend/src/components/_Atomic/imageslide/Slider_image.js
@@ -1,15 +1,15 @@
 export const Slider_image = [
     {
-        image: 'https://res.cloudinary.com/onlinepizza/image/upload/v1654039757/home/1635862099_bf6tzu.jpg'
+        image: './images/1635861992.jpg'
     },
         {
-        image: 'https://res.cloudinary.com/onlinepizza/image/upload/v1654039756/home/1635862028_l1o9ej.jpg'
+        image: './images/1635862028.jpg'
     },
     {
-        image: 'https://res.cloudinary.com/onlinepizza/image/upload/v1654039756/home/1635862068_jsnvme.jpg'
+        image: './images/1635862068.jpg'
     },
     {
-        image: 'https://res.cloudinary.com/onlinepizza/image/upload/v1654039756/home/1635861992_qjewio.jpg'
+        image: './images/1635862099.jpg'
     }
 
 ];
